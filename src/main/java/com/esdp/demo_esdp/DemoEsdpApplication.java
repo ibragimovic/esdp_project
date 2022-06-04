01b4ed24950e98c2075c938289c58b84e3318933
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-04 11:12:13 +0600

    added application

---
+++ src/main/java/com/esdp/demo_esdp/DemoEsdpApplication.java
@@ -0,0 +1,13 @@
+package com.esdp.demo_esdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEsdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEsdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esdp/demo_esdp/DemoEsdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdp.demo_esdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEsdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
