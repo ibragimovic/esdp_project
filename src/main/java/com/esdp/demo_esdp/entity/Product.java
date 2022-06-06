796ef57f6776ac7cc7f2621881e9f35c8408f4a3
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-07 01:22:48 +0600

    Update class Product

--- src/main/java/com/esdp/demo_esdp/entity/Product.java
+++ src/main/java/com/esdp/demo_esdp/entity/Product.java
@@ -1,4 +1,39 @@
 package com.esdp.demo_esdp.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Min(1)
+    private Integer price;
 }
