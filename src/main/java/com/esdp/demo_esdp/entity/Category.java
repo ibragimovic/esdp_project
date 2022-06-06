c6f91e31afd32a66728c47c3a61bd51935edf94d
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-07 01:24:46 +0600

    Update class Category

--- src/main/java/com/esdp/demo_esdp/entity/Category.java
+++ src/main/java/com/esdp/demo_esdp/entity/Category.java
@@ -1,4 +1,31 @@
 package com.esdp.demo_esdp.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "category")
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
 }
