0b84c6a473a1082a4dd3f91e8a7f35ef084a7c34
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-07 01:21:39 +0600

    Update class User

--- src/main/java/com/esdp/demo_esdp/entity/User.java
+++ src/main/java/com/esdp/demo_esdp/entity/User.java
@@ -1,5 +1,60 @@
-package main.java.com.esdp.demo_esdp.entity;
+package com.esdp.demo_esdp.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+
+    @NotBlank
+    private String lastname;
+
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String login;
+
+
+    @Size(min = 10)
+    private String telNumber;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @Column
+    @Builder.Default
+    private boolean enabled = true;
+
+
+    @NotBlank
+    @Size(min = 1, max = 128)
+    @Column(length = 128)
+    @Builder.Default
+    private String role = "USER";
+
 
 }
