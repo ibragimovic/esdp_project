cb5f9d68a2e115e2e73407ca4b4348d8ca1bce7e
AbdyrovElnar <eabdyrov@gmail.com>
2022-06-07 18:38:10 +0600

    user controller

---
+++ src/main/java/com/esdp/demo_esdp/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.esdp.demo_esdp.controller;
+
+import com.esdp.demo_esdp.exeption.UserRegisterForm;
+import com.esdp.demo_esdp.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/registration-page")
+    public String mainRegistrationPage()
+    {
+        return "registration-page";
+    }
+
+    @GetMapping("/profile")
+    public String pageCustomerProfile(Model model, Principal principal)
+    {
+        var user = userService.getByEmail(principal.getName());
+        model.addAttribute("dto", user);
+        return "profile";
+    }
+
+    @GetMapping("/register")
+    public String pageRegisterCustomer(Model model) {
+        if (!model.containsAttribute("dto")) {
+            model.addAttribute("dto", new UserRegisterForm());
+        }
+
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPage(@Valid UserRegisterForm userRequestDto,
+                               BindingResult validationResult,
+                               RedirectAttributes attributes) {
+        attributes.addFlashAttribute("dto", userRequestDto);
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/register";
+        }
+
+        userService.register(userRequestDto);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(@RequestParam(required = false, defaultValue = "false") Boolean error, Model model) {
+        model.addAttribute("error", error);
+        return "login";
+    }
+}
