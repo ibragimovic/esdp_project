6961c9d2ac046d5914272acadebfffb984c6acb0
AbdyrovElnar <eabdyrov@gmail.com>
2022-06-07 19:54:47 +0600

    do actualed Repository

--- src/main/java/com/esdp/demo_esdp/repositories/UserRepository.java
+++ src/main/java/com/esdp/demo_esdp/repositories/UserRepository.java
@@ -1,7 +1,14 @@
 package com.esdp.demo_esdp.repositories;
 
+
 import com.esdp.demo_esdp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
