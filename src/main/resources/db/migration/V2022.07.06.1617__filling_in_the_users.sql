be3c71ef72f8c3e46a1f16da72f8b8ccc69a264a
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-07 18:56:11 +0600

    added file migration product table

---
+++ src/main/resources/db/migration/V2022.07.06.1617__filling_in_the_users.sql
@@ -0,0 +1,7 @@
+insert into users ( name, lastname,tel_number, login, email, password,role)
+values ( 'petr','petrov','+9965071234567', 'petr001', 'petr@test.test', '$2a$10$uxFMwEr5526SkPj7qQAl4uEiBzOtA3GXLNQofJzYiFJuY7h.BvMXi','Admin'),
+       ( 'sasha', 'coreshcov','+9965071234567','sasha22', 'sanya@test.test', '$2a$10$6ZPqXoosmA2GRbOz3GHIU.n9UuF0L1yyiXiwp9dtXjV0GJWq2rYri','User'),
+       ( 'vasya', 'pupkin','+9965071234567','vas_00', 'vasya@test.test', '$2a$10$F01HT9Y/AlKUqGTwjLsWNOA7whWLohem7cAMsh0DPUSsGWr8pkOOO','User');
+
+
+--пароли у всех пользователей - 12345678
