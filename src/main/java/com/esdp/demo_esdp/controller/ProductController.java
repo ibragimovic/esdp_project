a20e80efb0cb1b1807dc0d86e505793afb3e056a
MusaevIbragim <m.ibragim88@gmail.com>
2022-06-10 17:32:28 +0600

    added to ProductController addNewProduct handler

--- src/main/java/com/esdp/demo_esdp/controller/ProductController.java
+++ src/main/java/com/esdp/demo_esdp/controller/ProductController.java
@@ -1,36 +1,60 @@
 package com.esdp.demo_esdp.controller;
 
-import com.esdp.demo_esdp.entity.Product;
+import com.esdp.demo_esdp.dto.ImageDTO;
+import com.esdp.demo_esdp.dto.ProductAddForm;
 import com.esdp.demo_esdp.entity.User;
 import com.esdp.demo_esdp.service.PropertiesService;
 import com.esdp.demo_esdp.service.UserService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
     private final PropertiesService propertiesService;
+    private final UserService userService;
+
+    @GetMapping("/product/add")
+    public String getAddPage(Model model) {
+        return "add-product";
+    }
+
+    @PostMapping("/product/add")
+    public String addNewProduct(@Valid ProductAddForm productAddForm,
+                                @Valid ImageDTO imageDTO,
+                                BindingResult validationResult,
+                                Principal principal,
+                                RedirectAttributes attributes) {
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/product/add";
+        }
+        propertiesService.addNewProduct(productAddForm, imageDTO, principal.getName());
+        return "redirect:/profile";
+    }
 
     @PostMapping("/product/delete")
     public String deleteProductById(@RequestParam("productId") Long productId,
-                                    Authentication authentication,
-                                    HttpSession httpSession) {
+                                    Authentication authentication) {
         User user = (User) authentication.getPrincipal();
-        propertiesService.deleteProductById(productId, user, httpSession);
+        propertiesService.deleteProductById(productId, user);
         return "redirect:/product";
     }
 
