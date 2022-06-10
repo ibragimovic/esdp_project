9c4b0282651ec195b72391e2446bcf360ed76a4c
atabekakbalaev <akbalaevatabek@gmail.com>
2022-06-10 17:55:15 +0600

    added tests for user repository

--- src/test/java/com/esdp/demo_esdp/user/UserRepositoryTests.java
+++ src/test/java/com/esdp/demo_esdp/user/UserRepositoryTests.java
@@ -4,9 +4,13 @@ import com.esdp.demo_esdp.entity.User;
 import com.esdp.demo_esdp.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTests {
     @Autowired
     private UserRepository userRepository;
@@ -15,6 +19,12 @@ public class UserRepositoryTests {
     @Test
     public void testUserGetById() {
         User user = userRepository.findById(1L).get();
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    public void testUserGetByEmail() {
+        User user = userRepository.findByEmail("petr@test.test").get();
         assertThat(user).isNotNull();
     }
 }
