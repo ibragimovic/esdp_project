033aa8f4385b3996b109aac81b50e3e032020cd8
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-15 19:09:18 +0600

    update migration file product

--- src/main/resources/db/migration/V2022.07.06.1619__add_product_table.sql
+++ src/main/resources/db/migration/V2022.07.06.1619__add_product_table.sql
@@ -1,19 +1,21 @@
 create table products
 (
-    id          bigserial not null
+    id             bigserial not null
         constraint products_pkey
             primary key,
-    description varchar(255),
-    name        varchar(255),
-    price       integer   not null
+    description    varchar(255),
+    name           varchar(255),
+    price          integer   not null
         constraint products_price_check
             check (price >= 1),
-    category_id bigint    not null
+    category_id    bigint    not null
         constraint fk1cf90etcu98x1e6n9aks3tel3
             references category,
-    user_id     bigint    not null
+    user_id        bigint    not null
         constraint fkdb050tk37qryv15hd932626th
             references users,
-    status      varchar(255),
-    data_add timestamp not null
+    status         varchar(255),
+    data_add       timestamp not null,
+    end_of_payment timestamp not null,
+    localities     varchar(255)
 );
