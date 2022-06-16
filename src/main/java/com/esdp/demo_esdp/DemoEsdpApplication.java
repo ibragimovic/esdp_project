c3e9b072572dec23d08d7bf09840e288403ab702
atabekakbalaev <akbalaevatabek@gmail.com>
2022-06-16 17:41:31 +0600

    added application freemarker config in constructor

--- src/main/java/com/esdp/demo_esdp/DemoEsdpApplication.java
+++ src/main/java/com/esdp/demo_esdp/DemoEsdpApplication.java
@@ -2,10 +2,15 @@ package com.esdp.demo_esdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class DemoEsdpApplication {
-
+    public DemoEsdpApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
+        freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(singletonList("/META-INF/security.tld"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoEsdpApplication.class, args);
     }
