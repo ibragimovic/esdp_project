4532401b2150bc6c876159fd65e00523a407373c
SmaginVladimir <smagin.vovan09@gmail.com>
2022-06-23 18:27:36 +0600

    update AdminController

--- src/main/java/com/esdp/demo_esdp/controller/AdminController.java
+++ src/main/java/com/esdp/demo_esdp/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.esdp.demo_esdp.controller;
 
-import com.esdp.demo_esdp.repositories.UserRepository;
 import com.esdp.demo_esdp.service.ProductService;
 import com.esdp.demo_esdp.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +15,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
     private final UserService userService;
     private final ProductService productService;
-    private final UserRepository userRepository;
 
 
     @GetMapping()
     public String getAdmin(Model model) {
+        model.addAttribute("products",productService.getProductsAll());
         return "admin";
     }
 
 
     @GetMapping("/users")
     public String getAdminUsers(Model model){
-        model.addAttribute("users",userRepository.findAll());
+        model.addAttribute("users",userService.getUsers());
         return "admin_users";
     }
 
 
     @PostMapping("/user-delete")
     public String deleteUserId(Long id){
-        userRepository.deleteById(id);
+        userService.deleteUser(id);
         return "redirect:/admin/users";
+    }
+
+
+    @PostMapping("/product-delete")
+    public String deleteProductId(Long id){
+        productService.deleteProductId(id);
+        return "redirect:/admin";
+    }
+
+
+    @PostMapping("/product-update-status")
+    public String updateStatusProduct(String status ,Long id){
+        productService.updateProductStatusId(status, id);
+        return "redirect:/admin";
     }
 }
