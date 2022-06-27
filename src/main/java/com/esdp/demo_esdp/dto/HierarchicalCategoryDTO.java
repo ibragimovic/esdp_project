66807b1920e6e844ffeb90ad87624024dc693810
atabekakbalaev <akbalaevatabek@gmail.com>
2022-06-27 12:35:02 +0600

    added hiearchical category dto

--- src/main/java/com/esdp/demo_esdp/dto/HierarchicalCategoryDTO.java
+++ src/main/java/com/esdp/demo_esdp/dto/HierarchicalCategoryDTO.java
@@ -1,4 +1,26 @@
 package com.esdp.demo_esdp.dto;
 
+import com.esdp.demo_esdp.entity.Category;
+import com.esdp.demo_esdp.repositories.CategoryRepository;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Data
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class HierarchicalCategoryDTO {
+    private Long id;
+    private String name;
+    private List<HierarchicalCategoryDTO> subCategories;
+
+    public static HierarchicalCategoryDTO from(Category category) {
+        return builder()
+                .id(category.getId())
+                .name(category.getName())
+                .subCategories(null)
+                .build();
+    }
 }
