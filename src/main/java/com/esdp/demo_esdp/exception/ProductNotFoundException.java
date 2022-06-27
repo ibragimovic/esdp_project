995466a5944d97f16a9eaa3e86728e851ed5ef45
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-06-27 14:01:59 +0600

    add exception and controller advice for catch exceptions by product

---
+++ src/main/java/com/esdp/demo_esdp/exception/ProductNotFoundException.java
@@ -0,0 +1,19 @@
+package com.esdp.demo_esdp.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@Getter
+@Setter
+public class ProductNotFoundException extends Throwable {
+    private String resource;
+    private String name;
+
+    public ProductNotFoundException(String resource, String name) {
+        this.resource = resource;
+        this.name = name;
+    }
+}
