5aa193763e9a97ad99cf0c2083528cbc7a135239
MusaevIbragim <m.ibragim88@gmail.com>
2022-07-04 16:43:50 +0600

    Updated users table

--- src/main/resources/db/migration/V2022.07.06.1616__add_users_table.sql
+++ src/main/resources/db/migration/V2022.07.06.1616__add_users_table.sql
@@ -5,10 +5,10 @@ create table users
             primary key,
     name     varchar   not null,
     lastname    varchar   not null,
-    tel_number    varchar   not null,
+    tel_number    varchar,
     login    varchar   not null,
     email    varchar   not null,
-    password varchar   not null,
+    password varchar,
     enabled boolean NOT NULL default true,
     role varchar(16) NOT NULL default 'USER',
     activation_code varchar
