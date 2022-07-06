8b8e1a598b2ec24f55342bb00d66878b00805d89
SmaginVladimir <smagin.vovan09@gmail.com>
2022-07-06 18:29:50 +0600

    update SecurityConfig

--- src/main/java/com/esdp/demo_esdp/config/SecurityConfig.java
+++ src/main/java/com/esdp/demo_esdp/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/admin/**")
-                .hasAnyAuthority("Admin");
+                .hasAnyAuthority("Admin")
+                .and().exceptionHandling().accessDeniedPage("/");
 
         http.authorizeRequests()
                 .anyRequest()
