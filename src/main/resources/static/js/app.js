ea6b889bcefc821ca426360fd327c7b404908224
atabekakbalaev <akbalaevatabek@gmail.com>
2022-07-09 17:41:11 +0600

    add localStorage for categories with expiry date

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    var categories_data = new Object();
+    var raw_categories_data = localStorage.getItem("categories_data");
+    if (raw_categories_data == null) {
+        renewCategoriesLocalStorage(categories_data);
+    } else {
+        categories_data = JSON.parse(raw_categories_data);
+        var categories = categories_data.categories;
+        var expire = categories_data.expire;
+        var now = new Date();
+        var oneDay = 1000 * 60 * 60 * 24;
+        if(now - expire > oneDay) {
+            renewCategoriesLocalStorage(categories_data);
+        }
+    }
+
     $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
       if (!$(this).next().hasClass('show')) {
         $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
@@ -12,3 +27,17 @@ $(document).ready(function() {
       return false;
     });
 });
+
+var renewCategoriesLocalStorage = function(categories_data) {
+    var jqxhr = $.get( "/categories", function(data) {
+        }).done(function(data) {
+            var expire = new Date();
+            expire.setDate(expire.getDate() + 1);
+            categories_data.categories = data;
+            categories_data.expire = expire;
+            localStorage.setItem("categories_data", JSON.stringify(categories_data));
+        }).fail(function() {
+            console.log( "network error" );
+        }).always(function() {
+        });
+};
