4b33a411c559d9932028ff6a577bb5f8a8f290c4
MusaevIbragim <m.ibragim88@gmail.com>
2022-07-29 11:59:11 +0600

    deleted FreeMarker config from DemoEsdpApplication class

--- src/main/java/com/esdp/demo_esdp/DemoEsdpApplication.java
+++ src/main/java/com/esdp/demo_esdp/DemoEsdpApplication.java
@@ -1,21 +1,11 @@
 package com.esdp.demo_esdp;
 
-import freemarker.ext.jsp.TaglibFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-
-import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class DemoEsdpApplication {
-    public DemoEsdpApplication(FreeMarkerConfigurer freeMarkerConfigurer) {
-        freeMarkerConfigurer.getTaglibFactory().setClasspathTlds(singletonList("/META-INF/security.tld"));
-        TaglibFactory tagLibFactory = freeMarkerConfigurer.getTaglibFactory();
-        if (tagLibFactory.getObjectWrapper() == null) {
-            tagLibFactory.setObjectWrapper(freeMarkerConfigurer.getConfiguration().getObjectWrapper());
-        }
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoEsdpApplication.class, args);
     }
